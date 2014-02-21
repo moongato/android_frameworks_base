957f6c932cd02c2be86daa7a1236023ec4f06da3
moongato <moongato@ubuntu>
2014-02-21 10:26:54 -0800

    Revert "[1/3] base: allow disabling non intrusive incall ui"
    This reverts commit f4762ce11e34679e5585ab3f33e909e73cc7ac3b.
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2619,13 +2619,6 @@ public final class Settings {
         public static final String LOCKSCREEN_TARGETS = "lockscreen_targets";
 
         /**
-         * Should the non-intrsive incall ui be used
-         *
-         * @hide
-         */
-        public static final String NON_INTRUSIVE_INCALL = "non_intrusive_incall";
-
-        /**
          *  Enable statusbar double tap gesture on to put device to sleep
          * @hide
          */
